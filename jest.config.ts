2000a593fe27afd25ac95d44c03034189606582a
josekabo <a@xdevs.com>
2023-05-17 18:26:42 -0600

    init scrapper

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from '@jest/types';
+// Sync object
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  clearMocks: true,
+  testMatch: ['**/__tests__/*.test.ts'],
+  verbose: true,
+  coverageReporters: ['lcov', 'text-summary', 'text'],
+  coverageThreshold: {
+    global: {
+      lines: 80,
+      branches: 65,
+      functions: 80,
+    },
+  },
+  collectCoverageFrom: ['src/**/*'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  setupFiles: [
+    'dotenv/config',
+    //'./__tests__/__mocks__/*.ts',
+  ],
+  moduleDirectories: ['node_modules', 'src'],
+};
+export default config;
